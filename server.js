7e1517949048b85d74922049a27a0ab15415d75b
Josh Wilson <joshua.wilson6289@gmail.com>
2020-11-17 12:00:06 -0600

    trying to get mongo db set up properly in robo 3t

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+const databaseUrl = "workouttracker";
+const collections = ["workouts"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
 app.listen(3000, () => {
   console.log("App running on port 3000!");
 });
