962381c1b2c1d626d208d0bbd750b9821a8f924f
Josh Wilson <joshua.wilson6289@gmail.com>
2020-11-19 14:19:42 -0600

    changed PORT variable for heroku

--- server.js
+++ server.js
@@ -114,6 +114,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "./public/index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("App running on port 3000!");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Now listening on: " + PORT));
