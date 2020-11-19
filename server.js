115f9a28f2242963bd24583c00e7e72c4e6cbde7
Josh Wilson <joshua.wilson6289@gmail.com>
2020-11-19 16:34:59 -0600

    changing mongoose.connect again in server.js

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
   useFindAndModify: false
 });
 
