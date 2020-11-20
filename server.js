c7933d9a9b7fc3028aff81fcff2144ad53cce5d3
Josh Wilson <joshua.wilson6289@gmail.com>
2020-11-20 16:59:17 -0600

    trying to take out useCreateIndex and useFindAndModify

--- server.js
+++ server.js
@@ -27,13 +27,11 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    useUnifiedTopology: true
+    // useCreateIndex: true,
+    // useFindAndModify: false
   }
 );
-
-module.exports = {mongoose};
 
 db.on("error", error => {
   console.log("Database Error:", error);
