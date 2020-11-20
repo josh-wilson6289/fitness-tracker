31dbc516a39b27255c39d1fdfc471880daab892e
Josh Wilson <joshua.wilson6289@gmail.com>
2020-11-20 17:00:57 -0600

    didn't work, adding back in

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-    // useCreateIndex: true,
-    // useFindAndModify: false
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   }
 );
 
